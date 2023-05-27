a89b90e9454d55dd52599a5eb697b574a77f4527
Alef oliveira <alefolivfeira@gmail.com>
2023-05-27 10:18:06 -0300

    di

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
   darkModeToggle.addEventListener("click", () => {
     body.classList.toggle("dark-mode");
+    var modalContainer = document.getElementById("modal-container");
+    modalContainer.classList.toggle("dark-mode");
   });
 });
 
@@ -41,12 +43,13 @@ xhr.onload = function () {
 xhr.send();
 
 document.addEventListener("DOMContentLoaded", function () {
+  var loadingElement = document.getElementById("loading");
   var projectsContainer = document.querySelector(".projects");
 
   // Função para obter os dados dos projetos
   function getProjects() {
-    // Adicionar a classe 'loading' para exibir o efeito de skeleton loading
-    projectsContainer.classList.add("loading");
+    // Exibir o elemento de loading
+    loadingElement.style.display = "flex";
 
     // Fazer a requisição para a API do GitHub
     var xhr = new XMLHttpRequest();
@@ -55,10 +58,10 @@ document.addEventListener("DOMContentLoaded", function () {
       if (xhr.status >= 200 && xhr.status < 400) {
         var projects = JSON.parse(xhr.responseText);
 
-        // Limpar o conteúdo atual
+        // Limpar o conteúdo atual dos projetos
         projectsContainer.innerHTML = "";
 
-        // Iterar sobre os projetos e exibir no formato HTML
+        // Iterar sobre os projetos e criar as caixas
         projects.forEach(function (project) {
           var projectBox = document.createElement("div");
           projectBox.classList.add("project-box");
@@ -78,12 +81,47 @@ document.addEventListener("DOMContentLoaded", function () {
           projectBox.appendChild(projectLink);
 
           projectsContainer.appendChild(projectBox);
+
+          projectBox.addEventListener("click", function () {
+            showModal(project);
+          });
+
+          window.addEventListener("click", function (event) {
+            if (event.target.classList.contains("modal-container")) {
+              closeModal();
+            }
+          });
+
+          document
+            .getElementById("modal-container")
+            .addEventListener("click", closeModal);
         });
       }
+      // Remover o elemento de loading
+      loadingElement.style.display = "none";
     };
     xhr.send();
   }
-
-  // Chamar a função para obter os projetos
   getProjects();
 });
+
+function showModal(project) {
+  document.getElementById("modal-title").textContent = project.name;
+  document.getElementById("modal-description").textContent =
+    project.description;
+  document.getElementById("modal-stars").textContent =
+    "Estrelas: " + project.stargazers_count;
+  document.getElementById("modal-owner").textContent =
+    "Dono: " + project.owner.login;
+  document.getElementById("modal-followers").textContent =
+    "Seguidores: " + project.owner.followers;
+
+  // Exibir o modal
+  document.getElementById("modal-container").style.display = "block";
+}
+
+function closeModal() {
+  var modalContainer = document.getElementById("modal-container");
+  modalContainer.classList.remove("dark-mode");
+  modalContainer.style.display = "none";
+}
